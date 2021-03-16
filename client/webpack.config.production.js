82f076dcc8dc83f41402e81eec08e9d6c5ea57f1
Martin Othamar <martin@othamar.net>
2021-03-16 21:48:11 +0100

    update domain

--- client/webpack.config.production.js
+++ client/webpack.config.production.js
@@ -61,7 +61,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV) || JSON.stringify('production'),
-        API_URL: JSON.stringify(process.env.API_URL) || JSON.stringify('https://jira-clone.mad.itera.no'),
+        API_URL: JSON.stringify(process.env.API_URL) || JSON.stringify('https://jira-clone-api.mad.itera.no'),
       },
     }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
